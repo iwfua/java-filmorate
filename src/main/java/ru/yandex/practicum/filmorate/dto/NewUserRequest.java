80bd2501d732467bfd82984d4360bd5494f919d1
Nikolay Zuborev <kolyasik200210@yandex.ru>
2024-07-22 18:45:49 +0300

    added dto

---
+++ src/main/java/ru/yandex/practicum/filmorate/dto/NewUserRequest.java
@@ -0,0 +1,14 @@
+package ru.yandex.practicum.filmorate.dto;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class NewUserRequest {
+    private String name;
+    private String email;
+    private String login;
+    private LocalDate birthday;
+}
+
